ce9da2a96ffd7e0e742f836d32c68a725ac70e41
Jarana Manotumruksa <feay1234@gmail.com>
2020-07-01 22:30:50 +0100

    handle gpus

--- MultiWoz.py
+++ MultiWoz.py
@@ -10,6 +10,15 @@ from tokenizers import BertWordPieceTokenizer
 from tqdm import tqdm
 from transformers import BertTokenizer, TFBertModel, BertConfig
 
+
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
 max_len = 512
 configuration = BertConfig()  # default paramters and configuration for BERT
 
