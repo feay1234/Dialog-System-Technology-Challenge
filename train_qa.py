f1499333bb9b388c713b9b9cf039e5e627fe5171
feay1234 <feay1234@gmail.com>
2020-07-16 11:02:32 +0100

    rename

--- train_qa.py
+++ train_qa.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset = args.data_root.split("/")[1]
-    modelName = "dst_qa_short"
+    modelName = "dst_qa_random"
     timestamp = strftime('%Y_%m_%d_%H_%M_%S', localtime())
 
     filename = "%s_%s_e%d_%s" % (dataset, modelName, args.n_epochs, timestamp)
